a02b8a3e5c1b5ac7e720cdcb9269bec895846c66
shiftywitch <johbirk00@gmail.com>
2021-02-03 19:27:37 +0100

    Fixed some stuff(?) That Even forgot

--- src/main/java/editor/Document.java
+++ src/main/java/editor/Document.java
@@ -1,7 +1,5 @@
 package editor;
 
-spackage editor;
-
 import editor.display.CharacterDisplay;
 
 /**
@@ -16,56 +14,173 @@ import editor.display.CharacterDisplay;
  * @author evenal
  */
 
-
 public class Document {
 
-    /** Document data structure(s) */
-    // pointer to the
+    /** The doc is a list of lines */
+    Line sentinel;
+    Line currentLine;
+    Line firstVisible;
+
     CharacterDisplay display;
 
     public Document(CharacterDisplay display) {
-        //set up data structure
+        sentinel = new Line();
+        currentLine = new Line();
+        firstVisible = currentLine;
+        sentinel.next = currentLine;
+        currentLine.next = sentinel;
+        sentinel.prev = currentLine;
+        currentLine.prev = sentinel;
 
         this.display = display;
     }
 
-    private void updateDisplay() {
-        // should be called at the end of the functionality
-        // and should update the display
+    public void print() {
+        for (Line line = sentinel.next;
+             line != sentinel;
+             line = line.next) {
+            line.print();
+        }
     }
 
-    /*
-     * The following methods are called from the actions. Decide on
-     * the data structure(s) for Document first. Then finish these
-     * methods
-     */
+    private void updateDisplay() {
+        int lineCount = 0;
+
+        for (Line line = firstVisible;
+             line != sentinel
+                     && lineCount < display.getHeight();
+             line = line.next) {
+            lineCount++;
+            line.updateDisplay(lineCount);
+        }
+    }
+
     public void insertLine() {
-        // create a new line in the data structure
-        updateDisplay();
+
     }
 
     public void insert(Character c) {
-        // insert the character c into the data structure
-
+        currentLine.insert(c);
         updateDisplay();
-    }
-
-    public char deleteNext() {
-    }
-
-    public char deletePrev() {
     }
 
     public void moveCursor(String direction) {
 
     }
 
-    private void updateDisplay(int line) {
-        // for all visible characterso
-        // show them in the rightplace
-        display.displayChar(c, line, column);
-
-        // and make the cursor stand out a little
-        display.displayCursor(c, line, column);
+    public void deleteNext() {
+        currentLine.deleteNext();
     }
+
+    public void deletePrev() {
+        currentLine.deletePrev();
+
+    }
+
+    /**
+     * There is one instance of this class for every character in the
+     * buffer. Each Line object contains a double linked list of
+     * CharNodes (using the prev and next pointers in this class
+     */
+    private class CharNode {
+
+        CharNode prev, next;
+        char c;
+
+        public CharNode() {
+            c = 0;
+            prev = this;
+            next = this;
+        }
+
+        public CharNode(char c,
+                        CharNode prev,
+                        CharNode next) {
+            this.c = c;
+            this.prev = prev;
+            this.next = next;
+        }
+
+        public void insert(char c) {
+            CharNode n = new CharNode(c, this, next);
+            this.next = n;
+            n.next.prev = n;
+        }
+
+        private void deletePrev() {
+            if (prev == currentLine.sentinel) {
+                // careful....
+            }
+            else {
+                prev = prev.prev;
+                prev.next = this;
+            }
+        }
+
+        private void deleteNext() {
+
+        }
+    }
+
+    /**
+     * Each Line object contains a double linked list of CharNode
+     * objects, which hold the characters in the line.
+     *
+     * One of the lines contains the cursor. Logically the cursor is
+     * always between two characters. This is not possible, so the
+     * cursor field points to the character (CharNode object) just
+     * behind the cursor. Characters are always inserted between the
+     * cursor and the next node.
+     */
+    private class Line {
+
+        CharNode sentinel;
+        CharNode cursor;
+        Line prev;
+        Line next;
+
+        public Line() {
+            sentinel = new CharNode();
+            cursor = sentinel;
+            prev = this;
+            next = this;
+        }
+
+        private void insert(char c) {
+            assert cursor != null;
+            cursor.insert(c);
+            cursor = cursor.next;
+        }
+
+        private void deletePrev() {
+            cursor.deletePrev();
+        }
+
+        private void deleteNext() {
+            cursor.deleteNext();
+        }
+
+        private void print() {
+            for (CharNode cn = sentinel.next;
+                 cn != sentinel;
+                 cn = cn.next) {
+                System.out.format("%c", cn.c);
+            }
+        }
+
+        private void updateDisplay(int line) {
+            int col = 0;
+            for (CharNode cn = sentinel.next;
+                 cn != sentinel;
+                 cn = cn.next) {
+                if (cn == cursor)
+                    display.displayCursor(cn.c, line, col);
+                else
+                    display.displayChar(cn.c, line, col);
+                col++;
+            }
+        }
+    }
+
 }
+
