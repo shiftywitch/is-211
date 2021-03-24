5f382a3f8081ebd77176e9742f0e6172181b114e
shiftywitch <johbirk00@gmail.com>
2021-03-24 12:53:34 +0100

    inserted kjartan's version a2v2()

--- src/main/java/assignment2/Assignment2.java
+++ src/main/java/assignment2/Assignment2.java
@@ -1,14 +1,18 @@
 package assignment2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
+import java.util.*;
 
 public class Assignment2 {
     public static void main(String[] args) {
         int[] numbers = {4,2,7,6,-3,-1,-2,42,0,-42,9,-4,5,-5,-6,-7,-8,-99,42,11,20,1,2,3};
 
+        //Task 2A version 1
         System.out.println(a2(numbers));
+
+        //Task 2A version 2
+        a2v2(numbers);
+
+
     }
 
     //Consecutive subStrings that adds up to zero
@@ -27,15 +31,50 @@ public class Assignment2 {
             }
 
             while (remaining.hasNext()) {
-                int next = remaining.next();
-                sum += next;
-                consecutiveSubString.append(next).append(" ");
                 if (sum == 0) {
                     validConsecutiveSubStrings.add(consecutiveSubString.toString());
                 }
+                int next = remaining.next();
+                sum += next;
+                consecutiveSubString.append(next).append(" ");
             }
         }
 
         return validConsecutiveSubStrings;
+    }
+
+    public static void a2v2(int[] A)
+    {
+        Map<Integer, List<Integer>> hashMap = new HashMap<>();
+
+        insert(hashMap, 0, -1);
+
+        int sum = 0;
+
+        for (int i = 0; i < A.length; i++)
+        {
+            sum += A[i];
+
+            if (hashMap.containsKey(sum))
+            {
+                List<Integer> list = hashMap.get(sum);
+
+                // find all subarrays with the same sum
+                for (Integer value: list)
+                {
+                    System.out.println("Subarray [" + (value + 1) + "…" +
+                            i + "]");
+                }
+            }
+
+            // insert (sum so far, current index) pair into the hashmap
+            insert(hashMap, sum, i);
+        }
+    }
+
+    private static<K, V> void insert(Map<K, List<V>> hashMap, K key, V value)
+    {
+        hashMap.putIfAbsent(key, new ArrayList<>());
+        hashMap.get(key).add(value);
     }
 }
